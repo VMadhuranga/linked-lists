ea228e375e50968900566e860706513e629f9c40
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-09-08 14:39:21 +0530

    Update LinkedList class
    Create size method
    Add test cases

--- linked-lists.js
+++ linked-lists.js
@@ -44,8 +44,32 @@ class LinkedList {
 
     temp.NextNode = new Node(data);
   }
+
+  size() {
+    const tempArray = [];
+
+    let temp = this.#list;
+
+    while (temp.NextNode !== null) {
+      tempArray.push(temp);
+      temp = temp.NextNode;
+    }
+
+    tempArray.push(temp);
+
+    return tempArray.length;
+  }
 }
 
 const newList = new LinkedList();
 
-console.log(newList);
+// append and prepend data
+newList.append("hello1");
+newList.append("hello2");
+newList.append("hello3");
+newList.prepend("hello4");
+newList.prepend("hello5");
+newList.prepend("hello6");
+
+// check for list size
+console.log(newList.size());
