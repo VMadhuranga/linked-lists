14100896a1f9663dcb50be5dacd4881ac9ef014b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-09-08 14:36:01 +0530

    Update Node class
    Create getters and setters for data and nextNode fields

--- linked-lists.js
+++ linked-lists.js
@@ -4,6 +4,23 @@ class Node {
   #data;
 
   #nextNode;
+
+  constructor(data, nextNode = null) {
+    this.#data = data;
+    this.#nextNode = nextNode;
+  }
+
+  get Data() {
+    return this.#data;
+  }
+
+  get NextNode() {
+    return this.#nextNode;
+  }
+
+  set NextNode(node) {
+    this.#nextNode = node;
+  }
 }
 
 class LinkedList {
