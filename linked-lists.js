17186cf5d1559ca325fef345673f47bc4ae6e3d1
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-09-08 14:33:37 +0530

    Create LinkedList and Node class

---
+++ linked-lists.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+/* eslint-disable max-classes-per-file */
+class Node {
+  #data;
+
+  #nextNode;
+}
+
+class LinkedList {
+  #list = null;
+}
+
+const newList = new LinkedList();
+
+console.log(newList);
