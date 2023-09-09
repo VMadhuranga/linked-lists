531797c8e56f6701daf3ee07ad6b592cd378b615
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-09-09 10:10:50 +0530

    Update LinkedLists class
    Create insertAt method

--- linked-lists.js
+++ linked-lists.js
@@ -165,45 +165,64 @@ class LinkedList {
 
     return tempArray.join(" -> ");
   }
+
+  insertAt(data, index) {
+    if (this.#list === null || index === 0) {
+      this.prepend(data);
+      return;
+    }
+    const node = new Node(data);
+
+    if (this.at(index) !== "Element not found") {
+      const nodeAtCurIndex = this.at(index);
+      const nodeAtPreIndex = this.at(index - 1);
+      node.NextNode = nodeAtCurIndex;
+      nodeAtPreIndex.NextNode = node;
+      return;
+    }
+
+    console.log(this.at(index));
+  }
 }
 
 const newList = new LinkedList();
 
 // append and prepend data
-newList.append("hello1");
-newList.append("hello2");
-newList.append("hello3");
-newList.prepend("hello4");
-newList.prepend("hello5");
-newList.prepend("hello6");
+// newList.append("hello1");
+// newList.append("hello2");
+// newList.append("hello3");
+// newList.prepend("hello4");
+// newList.prepend("hello5");
+// newList.prepend("hello6");
 
 // check for list size
-console.log(newList.size());
+// console.log(newList.size());
 
 // check for first element in list
-console.log(newList.head());
+// console.log(newList.head());
 
 // check for last element in list
-console.log(newList.tail());
+// console.log(newList.tail());
 
 // check for element at given index
-console.log(newList.at(2));
+// console.log(newList.at(2));
 
 // remove last element from list
-newList.pop();
+// newList.pop();
 
 // check list size after removal
-console.log(newList.size());
+// console.log(newList.size());
 
 // append and prepend new values
-newList.append("helloA");
-newList.prepend("helloB");
+// newList.append("helloA");
+// newList.prepend("helloB");
 
-// check list contains passed data
-console.log(newList.contains("helloZ"));
+// // check list contains passed data
+// console.log(newList.contains("helloZ"));
 
-// find index of element contains data
-console.log(newList.find("hello1"));
+// // find index of element contains data
+// console.log(newList.find("hello1"));
 
-// log list data
-console.log(newList.toString());
+// newList.insertAt("helloP", 1);
+// // log list data
+// console.log(newList.toString());
